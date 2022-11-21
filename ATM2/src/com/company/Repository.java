14f65b42fd9e0eef751ee6e76be4d548d1c97afa
Gleb Goncharov <100234336+avvogg55@users.noreply.github.com>
2022-11-21 19:24:12 +0300

    Delete ATM2 directory

--- ATM2/src/com/company/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-import java.io.File;
-
-public class Repository {
-    public static final File dataBase = new File("G:\\JavaProjects\\ATM\\src\\com\\company\\notes3new.txt");
-    public static final File databse2 = new File("G:\\JavaProjects\\ATM\\src\\com\\company\\notes3.txt");
-
-}
